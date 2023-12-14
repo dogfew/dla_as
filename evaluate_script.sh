4ab8f5801266e5c2266730c7f3a342b128148ec6
dogfew <volodia.perepyolckin@gmail.com>
2023-12-14 22:03:54 +0300

    Final changes

--- evaluate_script.sh
+++ evaluate_script.sh
@@ -4,5 +4,5 @@ mkdir -p default_test_model
 cd default_test_model
 gdown 1UBJKIAn_ibl9UH7WmKzYpEphFkdz6V0z -O lcnn-lfcc-10.pth
 gdown 191cPrwnaGGu6Vz8-fEjr3U8NO3wkbSCt -O rawnet2-s3-50.pth
-gdown 1acqZie5JlQuJr7axjwzbC2Q8o-yRgYED -O rawnet2-s1.50.pth
+gdown 1acqZie5JlQuJr7axjwzbC2Q8o-yRgYED -O rawnet2-s1-50.pth
 cd ..
